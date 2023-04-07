a9eacd06d1460a8ca2465a91cfe771d691432f6f
Guillermo Riaño <guilledark17@gmail.com>
2023-04-07 12:07:04 -0600

    ✅ The Tower Class 1

--- Source/ToonTanks/Tower.cpp
+++ Source/ToonTanks/Tower.cpp
@@ -2,4 +2,30 @@
 
 
 #include "Tower.h"
+#include "Tank.h"
+#include "Kismet/GameplayStatics.h"
 
+// Called every frame
+void ATower::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    // Find the distance to the Tank
+    if (Tank)
+    {
+        float distance = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
+
+        // Check to see if the Tank is in range
+
+        // If in range, rotate turrent
+    }
+}
+
+// Called when the game starts or when spawned
+void ATower::BeginPlay()
+{
+	Super::BeginPlay();   
+
+    // Set Player 0 to the Tank
+    Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+}
--- Source/ToonTanks/Tower.h
+++ Source/ToonTanks/Tower.h
@@ -14,4 +14,14 @@ class TOONTANKS_API ATower : public ABasePawn
 {
 	GENERATED_BODY()
 	
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	class ATank* Tank;
 };
