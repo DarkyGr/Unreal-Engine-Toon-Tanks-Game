4079d10e5cb126235ee5cfb5d5cf3bb8664333fc
Guillermo Riaño <guilledark17@gmail.com>
2023-04-11 15:54:20 -0600

    🔨 Refactor Code: To Damage

--- Source/ToonTanks/BasePawn.cpp
+++ Source/ToonTanks/BasePawn.cpp
@@ -61,5 +61,6 @@ void ABasePawn::Fire()
 	// Spawn Projectile
 	FRotator Rotation = ProjectileSpawnPoint->GetComponentRotation();
 
-	GetWorld()->SpawnActor<AProjectile>(ProjectileClass, Location, Rotation);
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(ProjectileClass, Location, Rotation);
+	Projectile->SetOwner(this);
 }
